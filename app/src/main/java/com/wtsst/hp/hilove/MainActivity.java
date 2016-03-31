dbb36064a13129f59f2d067d9808ecc94bdd3923
sunshuntao <staryumou@163.com>
2016-03-31 14:30:43 +0800

    测试android studio中的快捷键，总结一下常用的快捷键 Window中和Mac中没有特别标明的代表相同快捷键。 1）导包：Alt+Enter ; 2）Format Code：CTRL_ALT+L; MAC : OPTION+CMD+L; 3）打开最近文件列表：CTRL+E; 4）跳到方法头尾：CTRL+}; 5）跳到上次编辑的地方：CTRL+SHIFT+BACKSPACE; 6）显示文件的结构：CTRL+F12; 7）快速打开文件：CTRL+N和CTRL+SHIFT+N； 8）构造器等内容：ALT+INSERT； 9）类名自动补全：CTRL+ALT+SPACE； 10）注释代码：CTRL+/ 和 CTRL+SHIFT+/；

--- app/src/main/java/com/wtsst/hp/hilove/MainActivity.java
+++ app/src/main/java/com/wtsst/hp/hilove/MainActivity.java
@@ -5,12 +5,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private String name = "wtsstlove1314520";
+
+    private Double ctrlaltspace = 12.13;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+
+
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
